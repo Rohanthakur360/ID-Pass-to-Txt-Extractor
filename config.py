346c33a705bd4a35f4efb6e8e9c81afef6dc75e3
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-03 13:42:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 29707621
 api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+bot_token = "6472474798:AAEPR6JMAj5Jw52Og5tHoPPxkgGanGuVIJA"
+auth_users = "1871785273"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "1871785273"
 owner_users = [int(num) for num in osowner_users.split(",")]
