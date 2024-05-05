f656232b402c727721ef0d8bc385d311170db219
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 11:44:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 28919553
 api_hash = "2ed4532cc8353ab77179cb291ffeb86e"
-bot_token = "kljaahrklaflkjaf45612a1f1a2f1a"
+bot_token = "6499994285:AAFaSySS9Fc50k9-CKvOPlZ5-Fcv7ZH7b28"
 auth_users = "1871785273"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "1871785273"
