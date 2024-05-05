de75b17fd1162fdae49156069dfcf1c81ed362cf
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 11:41:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 29707621
 api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
 bot_token = "kljaahrklaflkjaf45612a1f1a2f1a"
-auth_users = "afafafafa"
+auth_users = "1871785273"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "14481631521541"
+osowner_users = "1871785273"
 owner_users = [int(num) for num in osowner_users.split(",")]
