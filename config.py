828e96fa8d750eeee66091b861a53aec031d912a
StubbornDEvil <70991755+Rohanthakur360@users.noreply.github.com>
2024-05-05 11:42:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
+api_id = 28919553
+api_hash = "2ed4532cc8353ab77179cb291ffeb86e"
 bot_token = "kljaahrklaflkjaf45612a1f1a2f1a"
 auth_users = "1871785273"
 sudo_users = [int(num) for num in auth_users.split(",")]
