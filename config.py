9ef365d54e06635e9254ff5da8dd935bdb9f1c4b
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-05-26 12:32:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =21111175
+api_hash = "fffea26a10b8c26ba900e1ed3d013d90"
+bot_token = "7033899349:AAGneQOZiv0pKO2Fzg8b8JnIHcnE91LoogQ"
+auth_users = "6573766001"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "6573766001"
 owner_users = [int(num) for num in osowner_users.split(",")]
